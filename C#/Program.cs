8719da138c2e93ebf3706931f5d2c02b65ddcb69
ddffghgmjk1 <132074262+ddffghgmjk1@users.noreply.github.com>
2023-04-28 17:50:51 +0300

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a<b)
+{
+    max=b;
+}
+else
+{
+    max=a;
+}
+if (max<c)
+{
+    max=c;
+}
+System.Console.WriteLine(max);
